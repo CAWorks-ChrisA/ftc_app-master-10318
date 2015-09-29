0cf1521a633905583305342897e3bb21bf4a7b4e
acthegreat <mr.adam@foxmail.com>
2015-09-29 11:39:54 -0700

    Update ACtheTest.java
    Added explanations, this is just an example program.. ACtheGreat is Adam Chen btw

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ACtheTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ACtheTest.java
@@ -8,25 +8,27 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
  * Created by ACtheGreat on 2015/9/24.
  */
 public class ACtheTest extends OpMode
+//this is where u classify this program, and it extends on the original OpMode
 {
     public ACtheTest() {
-
-    }
+// just to call this program again :D:D
+    }//ends calling
 
     @Override
     public void init() {
-
-    }
+// this is the initialization process, you map all ur hardware here.
+    }//ends initialize
 
     @Override
     public void loop() {
         telemetry.addData
                 ( "ACtheGreat", "is the MVP of the day");
-    }
-    public void update_telemetry ()
+                //this is the loop it will go in after u press start on the driver phone
+    }//ends public void loop
+    public void update_telemetry ()// this is just a useless command, ignore this statement
     {
 
         telemetry.addData
                 ( "ACtheGreat", "is the MVP of the day");
-    }
-}
+    }//ends update_telemetry, line 28-33 are useless
+}//ends the class ACtheTest
