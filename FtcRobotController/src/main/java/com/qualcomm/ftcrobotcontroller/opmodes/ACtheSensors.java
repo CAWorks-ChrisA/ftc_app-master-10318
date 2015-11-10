bc85d317d729b1f0a0a00c759814d54ba8607f22
acthegreat <mr.adam@foxmail.com>
2015-11-09 16:29:25 -0800

    sensor update: color
    need to calibrate the color

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ACtheSensors.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ACtheSensors.java
@@ -1,6 +1,8 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.IrSeekerSensor;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
@@ -10,20 +12,32 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 public class ACtheSensors  extends OpMode {
 private TouchSensor touch;
 private OpticalDistanceSensor dds;
+private ColorSensor color;
+private IrSeekerSensor ir;
     @Override
     public void init() {
         touch = hardwareMap.touchSensor.get ("touch");
         dds = hardwareMap.opticalDistanceSensor.get ("dds");
+        color = hardwareMap.colorSensor.get ("color");
+        ir = hardwareMap.irSeekerSensor.get ("ir");
+
 
     }
 
     @Override
     public void loop() {
         if (touch.isPressed()){
-            telemetry.addData ("Ryan, I win", "you lose");
+            telemetry.addData ("gg", "wp");
         }
        dds.enableLed(touch.isPressed());
-        telemetry.addData ("distance....", +dds.getLightDetected());
+        telemetry.addData("distance....", +dds.getLightDetected());
+        telemetry.addData("color", color.argb());
+        telemetry.addData ("ir", ir.signalDetected());
+        telemetry.addData ("color2", color.alpha());
+        telemetry.addData ("color3 blue", color.blue());
+        telemetry.addData ("color4 green", color.green());
+        telemetry.addData ("color5 red", color.red());
 
     }
+
 }
