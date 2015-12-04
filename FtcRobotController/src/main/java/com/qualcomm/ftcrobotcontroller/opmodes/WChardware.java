dc2574b7dab7c9f1424526db627fa8b0b0ba609a
wchou1006 <wchou1006@gmail.com>
2015-12-03 16:21:37 -0800

    2nd half of 12/3/15

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/WChardware.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/WChardware.java
@@ -42,69 +42,100 @@ public class TCHSHardware extends OpMode
             rightMotor = null;
             telemetry.addData("ERROR", " right motor not detected  ):  ");
         }
-        try {leftBackMotor = hardwareMap.dcMotor.get ("motorleft");}
-        catch (Exception p_exeception) {
+        try {
+            leftBackMotor = hardwareMap.dcMotor.get("motorleft");
+        } catch (Exception p_exeception) {
             leftBackMotor = null;
             telemetry.addData("ERROR", " left back motor not detected  ):  ");
         }
-        try {rightBackMotor = hardwareMap.dcMotor.get ("motorleft");}
-        catch (Exception p_exeception) {
+        try {
+            rightBackMotor = hardwareMap.dcMotor.get("motorleft");
+        } catch (Exception p_exeception) {
             rightBackMotor = null;
             telemetry.addData("ERROR", " right back motor not detected  ):  ");
         }
-        try {bucket = hardwareMap.dcMotor.get ("motorleft");}
-        catch (Exception p_exeception) {
+        try {
+            bucket = hardwareMap.dcMotor.get("motorleft");
+        } catch (Exception p_exeception) {
             bucket = null;
             telemetry.addData("ERROR", " bucket not detected  ):  ");
         }
-        try {conveyerBelt = hardwareMap.dcMotor.get ("motorleft");}
-        catch (Exception p_exeception) {
+        try {
+            conveyerBelt = hardwareMap.dcMotor.get("motorleft");
+        } catch (Exception p_exeception) {
             conveyerBelt = null;
             telemetry.addData("ERROR", " convyerbelt not detected  ):  ");
         }
-        try {servo1 = hardwareMap.servo.get ("servo1");}
-        catch (Exception p_exception) {
-            servo1 =null;
-            telemetry.addData("ERROR", "servo1 not detected ): " );
+        try {
+            servo1 = hardwareMap.servo.get("servo1");
+        } catch (Exception p_exception) {
+            servo1 = null;
+            telemetry.addData("ERROR", "servo1 not detected ): ");
         }
-        try {servo2 = hardwareMap.servo.get ("servo2");}
-        catch (Exception p_exception) {
-            servo2 =null;
-            telemetry.addData("ERROR", "servo2 not detected ): " );
+        try {
+            servo2 = hardwareMap.servo.get("servo2");
+        } catch (Exception p_exception) {
+            servo2 = null;
+            telemetry.addData("ERROR", "servo2 not detected ): ");
         }
-        try {servo3 = hardwareMap.servo.get ("servo3");}
-        catch (Exception p_exception)  {
+        try {
+            servo3 = hardwareMap.servo.get("servo3");
+        } catch (Exception p_exception) {
             servo3 = null;
-            telemetry.addData("ERROR", "servo3 not detected ): " );
+            telemetry.addData("ERROR", "servo3 not detected ): ");
 
         }
-        try {servo4 = hardwareMap.servo.get ("servo 4");}
-        catch (Exception p_exception) {
-            servo4= null;
+        try {
+            servo4 = hardwareMap.servo.get("servo 4");
+        } catch (Exception p_exception) {
+            servo4 = null;
             telemetry.addData("ERROR", "servo4 not detected ):");
         }
-        try {bucket = hardwareMap.dcMotor.get ("bucket motor");}
-        catch (Exception p_exception) {
-            bucket= null;
+        try {
+            bucket = hardwareMap.dcMotor.get("bucket motor");
+        } catch (Exception p_exception) {
+            bucket = null;
             telemetry.addData("ERROR", "Bucket Motor not detected ):");
         }
-        try {conveyerBelt = hardwareMap.dcMotor.get ("converyer belt");}
-        catch (Exception p_exception){
-            conveyerBelt= null;
+        try {
+            conveyerBelt = hardwareMap.dcMotor.get("converyer belt");
+        } catch (Exception p_exception) {
+            conveyerBelt = null;
             telemetry.addData("ERROR", "Conveyer Belt Motor not detected ):");
         }
-        try {ODS= hardwareMap.opticalDistanceSensor.get ("ODS");}
-        catch (Exception p_exctption){
-            ODS= null;
-            telemetry.addData("ERROR", "ODS is not detected ):" );}
-
+        try {
+            ODS = hardwareMap.opticalDistanceSensor.get("ODS");
+        } catch (Exception p_exception) {
+            ODS = null;
+            telemetry.addData("ERROR", "ODS is not detected ):");
+        }
+        try {
+            touch = hardwareMap.touchSensor.get("touch");
+        } catch (Exception p_exception) {
+            touch = null;
+            telemetry.addData("Error", "Touch is not detected ):");
+        }
 
 
         if (leftMotor != null){leftMotor.setDirection(DcMotor.Direction.REVERSE);}
         if (rightBackMotor != null){rightBackMotor.setDirection(DcMotor.Direction.REVERSE);}
     }
     @Override
-    public void loop() {
+    public void start ()
+    {
+        //
+        // Only actions that are common to all Op-Modes (i.e. both automatic and
+        // manual) should be implemented here.
+        //
+        // This method is designed to be overridden.
+        //
+    }
+    @Override
+    public void loop (){
+        //nothing to put in here.
+    }
+    @Override
+    public void telemetry_update ( ) {
 
     }
 }
