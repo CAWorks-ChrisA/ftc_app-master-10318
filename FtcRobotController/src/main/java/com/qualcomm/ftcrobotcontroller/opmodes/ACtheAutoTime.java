c0103dfe598cb61755197b990bcc1abae057ea23
acthegreat <mr.adam@foxmail.com>
2015-12-10 16:32:58 -0800

    autotime update
    Datas: 1.25 sec per 2 ft
    0.8-0.9 sec per 90 degree turn

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ACtheAutoTime.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ACtheAutoTime.java
@@ -5,15 +5,26 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  */
 public class ACtheAutoTime extends ACtheHardware{
     @Override public void loop(){
-        if (this.time <= 1 ){
+        if (this.time > 1 && this.time<= 2.25 ){
             set_drive_power(1.0, 1.0);
         }
-        else if (this.time > 1 && this.time<= 2){
+        else if (this.time >2.25 && this.time<= 3.15){
+          set_drive_power(-1.0, 1.0);
+        }
+        else if (this.time >3.15 && this.time <= 4.40){
+            set_drive_power(1.0, 1.0);
+        }
+        else if (this.time >4.40 && this.time <= 5.30){
+            set_drive_power(1.0, -1.0);
+        }
+        else if (this.time >5.30 && this.time <= 6.55){
+            set_drive_power(1.0, 1.0);
+        }
+        else if (this.time >6.55 && this.time <= 7.45){
             set_drive_power(-1.0, 1.0);
         }
-        else if (this.time >3 && this.time <= 4.75d){
-            set_drive_power(0.0, 0.0);
-            telemetry.addData("done", "mission accomplished");
+        else if (this.time >7.45 && this.time <= 8.35){
+            set_drive_power(1.0, 1.0);
         }
         else {
             set_drive_power(0.0, 0.0);
