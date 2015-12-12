c68b748b9e2a8f10568e3706d6e353840ce2a634
acthegreat <mr.adam@foxmail.com>
2015-12-11 16:44:14 -0800

    12/11/15 wesley
    nothing was changed and no tests were run because the hardware team was
    building

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ACtheAutoTime.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ACtheAutoTime.java
@@ -8,27 +8,10 @@ public class ACtheAutoTime extends ACtheHardware{
         //OK, this is ACtheGreat, fwd speed: 1.25 per 2 ft. turning speed.. ?
         // Tast for Wesley, test for turning speed:
         // last time, last result was 0.9 sec for 95-100 degrees, and please, test 0.8-0.85 seconds
-        if (this.time > 1 && this.time<= 2.25 ){
-            set_drive_power(1.0, 1.0);
-        }
-        else if (this.time >2.25 && this.time<= 3.15){
-          set_drive_power(-1.0, 1.0);
-        }
-        else if (this.time >3.15 && this.time <= 4.40){
-            set_drive_power(1.0, 1.0);
-        }
-        else if (this.time >4.40 && this.time <= 5.30){
-            set_drive_power(1.0, -1.0);
-        }
-        else if (this.time >5.30 && this.time <= 6.55){
-            set_drive_power(1.0, 1.0);
-        }
-        else if (this.time >6.55 && this.time <= 7.45){
+        if (this.time > 1 && this.time<= 1.8 ){
             set_drive_power(-1.0, 1.0);
         }
-        else if (this.time >7.45 && this.time <= 8.35){
-            set_drive_power(1.0, 1.0);
-        }
+
         else {
             set_drive_power(0.0, 0.0);
             telemetry.addData("Program", "stopped");
