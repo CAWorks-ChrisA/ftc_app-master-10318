0fd9c348535ba970643ec30b5128fef9d7818365
acthegreat <mr.adam@foxmail.com>
2015-12-14 17:09:29 -0800

    edits
    robot turning timing probs:
    0.7 testing currently

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ACtheAutoTime.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ACtheAutoTime.java
@@ -8,8 +8,11 @@ public class ACtheAutoTime extends ACtheHardware{
         //OK, this is ACtheGreat, fwd speed: 1.25 per 2 ft. turning speed.. ?
         // Tast for Wesley, test for turning speed:
         // last time, last result was 0.9 sec for 95-100 degrees, and please, test 0.8-0.85 seconds
-        if (this.time > 1 && this.time<= 1.8 ){
+        if (this.time > 1 && this.time<= 1.7 ){
             set_drive_power(-1.0, 1.0);
+        }
+        else if (this.time >1.7 && this.time <= 2.95){
+            set_drive_power(1.0, 1.0);
         }
 
         else {
