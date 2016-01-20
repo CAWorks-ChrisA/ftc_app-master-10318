89b4bd0328679577b418830c30f1b84d1877ceb6
acthegreat <mr.adam@foxmail.com>
2016-01-19 17:09:21 -0800

    new teleop

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Teleop.java
@@ -0,0 +1,64 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by TCHS Robotics on 1/19/2016.
+ */
+public class Teleop extends hardware{
+    @Override public void loop(){
+        //front is away from the bucket
+        set_drive_power(CLOCKWISE*gamepad1.left_stick_y , CLOCKWISE*gamepad1.right_stick_y);
+
+        //front is the bucket
+        if (gamepad1.dpad_up) {
+            set_drive_power(CLOCKWISE,CLOCKWISE);
+        }
+
+        if (gamepad1.dpad_down) {
+            set_drive_power(ANTICLOCKWISE,ANTICLOCKWISE);
+        }
+
+        if (gamepad1.dpad_left){
+            set_drive_power(CLOCKWISE,ANTICLOCKWISE);
+        }
+
+        if (gamepad1.dpad_right) {
+            set_drive_power(ANTICLOCKWISE,CLOCKWISE);
+        }
+
+        //belt
+        if (gamepad2.dpad_left) {
+            belt.setPower(CLOCKWISE);
+        }
+
+        if (gamepad2.dpad_right) {
+            belt.setPower(ANTICLOCKWISE);
+        }
+
+        //servos left side
+        if (gamepad2.a){
+            left.setPosition(0.2);//up
+        }
+
+        if (gamepad2.b){
+            left.setPosition(0.7);//down
+        }
+
+        //servos right side
+        if (gamepad2.x){
+            right.setPosition(0.2);//up
+        }
+
+        if (gamepad2.y){
+            right.setPosition(0.7);//down
+        }
+
+        //bucket
+        if (gamepad1.a) {
+            bucket.setPower(ANTICLOCKWISE);
+        }
+
+        if (gamepad1.b) {
+            bucket.setPower(CLOCKWISE);
+        }
+    }
+}
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/hardware.java
@@ -0,0 +1,64 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * TCHS ROBOTICS TEAM 10318
+ * RAMROID HARDWARE CLASS
+ */
+
+public class hardware extends OpMode {
+
+    //variable declaration
+    //rotation constants
+    final static double ANTICLOCKWISE = 1;
+    final static double CLOCKWISE = -1;
+    //driving:
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+    //belt
+    DcMotor belt;
+    //Servos
+    Servo left;
+    Servo right;
+    //bucket
+    DcMotor bucket;
+
+    @Override
+    public void init() {
+        //driving:
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        //belt
+        belt = hardwareMap.dcMotor.get("belt");
+
+        //servos
+        left = hardwareMap.servo.get("left");
+        right = hardwareMap.servo.get("right");
+
+        //bucket
+        bucket = hardwareMap.dcMotor.get("bucket");
+
+    }
+
+    @Override
+    public void loop() {
+        //designed to override, if needed.
+    }
+
+    public void set_drive_power(double left, double right){
+        frontLeft.setPower(left);
+        frontRight.setPower(right);
+        backLeft.setPower(left);
+        backRight.setPower(right);
+    }
+}
