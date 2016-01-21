c76685fad22912524e7241b3a03d8376b4b1805f
acthegreat <mr.adam@foxmail.com>
2016-01-21 15:37:22 -0800

    extra
    debug, please sync

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/teleoptest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/teleoptest.java
@@ -46,6 +46,8 @@ public class teleoptest extends OpMode{
                         state++;
                     }
                     break;
+                default:
+                    break;
             }
         }
 
